d0acee39a004320f9766679ef69d9c000ae3a865
Shubham Wani <sw21092000@gmail.com>
2026-03-11 19:34:32 +0530

    Update API base URL for production environment

--- script/api-config.js
+++ script/api-config.js
@@ -3,4 +3,6 @@ window.API_BASE_URL =
   window.location.hostname === "localhost" ||
   window.location.hostname === "127.0.0.1"
     ? "http://localhost:6060"
-    : "https://mechconnect-backend-render-1.onrender.com";
+     : "https://mechconnect-backend-render-1-fjvh.onrender.com";
+  
+    // : "https://mechconnect-backend-render-1.onrender.com";
